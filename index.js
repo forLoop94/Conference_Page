f1e7400f7bb67eb996bd1ac5ec6eccd51265005e
forLoop94 <charlesuwem94@gmail.com>
2023-04-22 12:18:40 +0100

    Add animations to mobile menu and headline section

--- index.js
+++ index.js
@@ -8,25 +8,15 @@ const menu = () => {
   const modal = document.createElement('section');
   modal.className = 'menu';
   modal.innerHTML = `<ul class="menu-list">
-  <div class="close flex">
-    <p>X</p>
-  </div>
-  <li><a class='hyperlink' href="index.html">Home <div class="middle-underline flex justify-center">
-      <div class="line"></div>
-    </div></a></li>
-  <li><a class='hyperlink' href="index2.html">About <div class="middle-underline flex justify-center">
-    <div class="line"></div>
-  </div></a></li>
-    <li><a class='hyperlink' href="index.html#program-section">Program <div class="middle-underline flex justify-center">
-      <div class="line"></div>
-    </div></a></li>
-    <li><a class='hyperlink' href="index.html#speakers-section">Featured Speakers <div class="middle-underline flex justify-center">
-      <div class="line"></div>
-    </div></a></li>
-    <li><a class='hyperlink' href="index2.html#partners">Sponsors <div class="middle-underline flex justify-center">
-      <div class="line"></div>
-    </div></a></li>
-  </ul>`;
+        <div class="close flex">
+          <p>X</p>
+        </div>
+        <li><a class='hyperlink' href="index.html">Home</a></li>
+        <li><a class='hyperlink' href="index2.html">About</a></li>
+          <li><a class='hyperlink' href="index.html#program-section">Program</a></li>
+          <li><a class='hyperlink' href="index.html#speakers-section">Speakers</a></li>
+          <li><a class='hyperlink' href="index2.html#partners">Sponsors </a></li>
+        </ul>`;
   main.appendChild(modal);
 
   const hyperlinks = document.querySelectorAll('.hyperlink');
@@ -71,45 +61,5 @@ const guestSpeakers = (arr) => {
   }
   return cardsContainer;
 };
-guestSpeakers(authors);
+guestSpeakers(fullAuthorList);
 
-const seeMore = document.createElement('div');
-
-seeMore.innerHTML = `<div class="see-more flex justify-center">
-<div>MORE <i class="fa fa-angle-down"></i></div>
-</div>`;
-
-const speakerSection = document.querySelector('#speakers-section');
-speakerSection.appendChild(seeMore);
-
-const seeMoreBtn = document.querySelector('.see-more');
-
-seeMoreBtn.addEventListener('click', () => {
-  guestSpeakers(seeMoreAuthors);
-  seeMore.remove();
-});
-
-function guestSpeakersDesktop(arr) {
-  for (let i = 0; i < arr.length; i += 1) {
-    const speakerDetails = arr[i];
-    const card = document.createElement('div');
-    card.className = 'cards';
-    card.classList.add('desktop-cards');
-    card.innerHTML = `<div class="image-container">
-    <img class="speaker-image" src=${speakerDetails.image.src} alt="Speaker's Image"/>
-    <img class="mozaic" src="images/mozaic_thumbnail (1).png" alt="Speaker's Image">
-  </div>
-    <div class="speaker-details">
-      <h3 class="sub-section-header-black">${speakerDetails.name}</h3>
-      <p class="sub-section-text-black red-color"><i>${speakerDetails.honours}</i></p>
-      <div class="middle-underline flex">
-        <div class="line-grey"></div>
-      </div>
-      <p class="sub-section-text-black">${speakerDetails.books}</p>
-    </div>`;
-    cardsContainer.appendChild(card);
-  }
-  return cardsContainer;
-}
-
-guestSpeakersDesktop(fullAuthorList);
